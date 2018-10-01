671a305e7a54fb7ee97ffcd6f594e801906f442b
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-01 13:56:40 +0200

    corrections

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Lamp from './Lamp';
-
 import Quotes from "./Quotes";
 
 class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      working: false
+    }
+}
+
+  handleClick = () => {
+    this.setState({
+      working: !this.state.working
+    });
+  };
+
+
   render() {
+    const turn = this.state.working?'stop':'work';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className= {turn} alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
-        <Lamp on />
-        <Lamp />
+        <button 
+          onClick = {this.handleClick}
+        >{turn.toUpperCase()}
+        </button>
         <Quotes/>
       </div>
     );
